93308987aed4a390fffc3f1e6903d4b627dea4f7
Sevinj0124 <Cavadova.24@icloud.com>
2024-05-23 17:54:14 +0400

    beautico

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,16 +3,17 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div className='footer container p-4'>
-<div className="row">
-    <p className='text-secondary text-right mt-2'>© Beautico by
+<> 
+    <section> <div className="row">
+<div className="left-side col-12 col-md-6 col-lg-6 p-5 ">
+<p className='text-secondary text-right mt-2'>© Beautico by
 Minimal Square.Powered by Webflow
 .</p>
-    <div className="col-12 col-md-4 ">
-        <div className="footer-icons  ">
-            <h4 className='text-dark justify-content-center '>Follow us</h4>
-            <div className="icons d-flex gap-3">
-                <a href="">
+</div>
+
+<div className="right-side col-12 col-md-6 col-lg-6 p-5 ">
+    <h3>Follow us</h3>
+<a href="">
                 <img src="/assets/images/Facebook.svg" alt="" />
                 </a>
                 <a href="">
@@ -22,13 +23,12 @@ Minimal Square.Powered by Webflow
                <a href="">
                <img src="/assets/images/Twitter.svg" alt="" />
                </a>
-            </div>
-        </div>
-      
-
-    </div>
 </div>
-     </div>
+    </div>
+
+    </section>
+   
+     </>
   )
 }
 
--- src/components/containers/Contact.jsx
+++ src/components/containers/Contact.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import'./Contact.css'
 
 const Contact = () => {
   return (
-    <div className='text-center'>Contact ucun</div>
+    <div className='row'>
+      <div className="left-side col-12 col-md-6 col-lg-6 p-5">
+<h2>Frequently asked  <br />questions</h2>
+<p className='text-secondary'>Frequently asked questions ordered by popularity. <br /> Remember that if the visitor has not committed to the call <br /> to action, they may still have questions (doubts) that can be  <br />answered.</p>
+<button className='contact p-2 text-white'>Contact us</button>
+      </div>
+
+      <div className="right-side col-12 col-md-6 col-lg-6">
+
+      </div>
+
+    </div>
   )
 }
 
